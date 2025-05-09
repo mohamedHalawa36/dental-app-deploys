bd6386051a557da65662b095a9176123d0199470
mohamedHalawa36 <133763026+mohamedHalawa36@users.noreply.github.com>
2025-05-09 21:51:27 +0000

    Deploying to main from @ mohamedHalawa36/dental-app@93cd0a4e3e5d1916176a1e7c859cd93ec367b6e1 🚀

---
+++ assets/auth-BbXAeHtL.js
@@ -0,0 +1,1 @@
+import{t as o}from"./index-TMktwTzD.js";import{s as t,A as e,j as n}from"./Input-B_bg18WL.js";const c=async r=>{const{data:a,error:s}=await t.auth.signInWithPassword(r);if(s)throw new e(s.message,s.status,s.code);return a},u=async()=>{const{error:r}=await t.auth.signOut();r&&o.error(n)};export{u as l,c as s};
