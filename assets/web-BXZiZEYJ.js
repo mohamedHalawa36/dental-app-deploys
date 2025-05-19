49641e7e12dc47e64ad52e73bc6379db5fb0f4bd
mohamedHalawa36 <133763026+mohamedHalawa36@users.noreply.github.com>
2025-05-19 15:00:31 +0000

    Deploying to main from @ mohamedHalawa36/dental-app@2779599b4da5c9c298e5892ce8498d167334ce3f 🚀

--- assets/web-BXZiZEYJ.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./PageLoader-DCvw-1SN.js";import"./supabase-D_SHXGLp.js";import"./chunk-SYFQ2XB5-BeKHOpyI.js";class t extends n{async show(e){}async hide(e){}}export{t as SplashScreenWeb};
