247f3f27061731e4f1f20fb920f6d75942c2b840
mohamedHalawa36 <133763026+mohamedHalawa36@users.noreply.github.com>
2025-05-24 21:45:10 +0000

    Deploying to main from @ mohamedHalawa36/dental-app@9e3d7536dc3d30be9adf4092b6217e6712c1aba4 🚀

--- assets/web-BSgNfkfh.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./useAttachBackBtn-BJZiXFsX.js";import"./chunk-SYFQ2XB5-BFxPMBKg.js";import"./supabase-ze08hsKw.js";class t extends n{async show(e){}async hide(e){}}export{t as SplashScreenWeb};
---
+++ assets/web-GX9JN-mH.js
@@ -0,0 +1,1 @@
+import{W as n}from"./useAttachBackBtn-CVzedvxQ.js";import"./chunk-SYFQ2XB5-BFxPMBKg.js";import"./supabase-4kc8f4RQ.js";class t extends n{async show(e){}async hide(e){}}export{t as SplashScreenWeb};
