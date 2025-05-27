261a80bb859381b1bfaacbc291b439027df7d656
mohamedHalawa36 <133763026+mohamedHalawa36@users.noreply.github.com>
2025-05-27 12:10:10 +0000

    Deploying to main from @ mohamedHalawa36/dental-app@f947aa4b9fb9e0085e8799dbc60b008aa44ee80c 🚀

--- assets/web-DabltLWh.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./useAttachBackBtn-BypiZuZT.js";import"./chunk-SYFQ2XB5-BeKHOpyI.js";import"./supabase-DFgKEEaP.js";class t extends n{async show(e){}async hide(e){}}export{t as SplashScreenWeb};
