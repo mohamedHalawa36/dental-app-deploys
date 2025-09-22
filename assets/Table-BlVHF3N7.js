020c8f05f677d6f9c744845811f2ecb4f621ba24
mohamedHalawa36 <133763026+mohamedHalawa36@users.noreply.github.com>
2025-09-22 14:17:24 +0000

    Deploying to main from @ mohamedHalawa36/dental-app@d9080304dd219f30557f75d2b92715769465dd21 🚀

--- assets/Table-BlVHF3N7.js
+++ assets/Table-BlVHF3N7.js
@@ -1,4 +1,4 @@
-import{r as P,p as w}from"./chunk-PVWAREVJ-chYXeG9N.js";import{l as F}from"./QueryClientProvider-Xy3iKmL1.js";import{B as z}from"./button-BhACotaA.js";/**
+import{r as P,p as w}from"./chunk-PVWAREVJ-chYXeG9N.js";import{l as F}from"./QueryClientProvider-Xy3iKmL1.js";import{B as z}from"./button-D8rxGqGp.js";/**
    * table-core
    *
    * Copyright (c) TanStack
